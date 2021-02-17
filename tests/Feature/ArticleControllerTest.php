d73cf4214cf3204e51add6a89aee736e269a13dd
Nonari Sanda <mkg_sandy@icloud.com>
2021-02-17 13:57:22 +0900

    ciテストの設定を完成しました

---
+++ tests/Feature/ArticleControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Tag;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ArticleControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testIndex()
+    {
+        $response = $this->get(route('articles.index'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.index');
+    }
+
+    public function create()
+    {
+        $allTagNames = Tag::all()->map(function ($tag) {
+            return ['text' => $tag->name];
+        });
+
+        return view('article.create', [
+            'allTagNames' => $allTagNames,
+        ]);
+    }
+
+    public function testGuestCreate()
+    {
+        $response = $this->get(route('articles.create'));
+
+        $response->assertRedirect(route('login'));
+    }
+
+    public function testAuthCreate()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('articles.create'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('articles.create');
+    }
+}
